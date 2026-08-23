de4dab5673a1438a62f3d744c6174b82d872a0d3
traviscomber <n3uralia@gmail.com>
2026-08-23 17:07:24 -0400

    feat(ui): integrate TDPI signals into monitoring

--- app/(app)/monitorear/page.tsx
+++ app/(app)/monitorear/page.tsx
@@ -1,1 +1,11 @@
-export { default } from "../../patentes/alertas/page"
+import PatentAlertsPage from "../../patentes/alertas/page"
+import { TdpiSignalInbox } from "@/components/intelligence/tdpi-signal-inbox"
+
+export default function MonitorearPage() {
+  return (
+    <>
+      <TdpiSignalInbox />
+      <PatentAlertsPage />
+    </>
+  )
+}
