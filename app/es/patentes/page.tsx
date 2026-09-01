2a9dc2df44b6982a954415fd2e5588041152dd56
traviscomber <n3uralia@gmail.com>
2026-09-01 16:00:16 -0400

    Complete Spanish patent keyboard navigation

--- app/es/patentes/page.tsx
+++ app/es/patentes/page.tsx
@@ -3,7 +3,13 @@ import { PublicPlatformNav } from "@/components/public-platform-nav"
 
 export const metadata = patentsMetadata("es")
 
-const patentReportLayoutFix = `
+const patentPublicFixes = `
+.patents-public-page a:focus-visible,
+.patents-public-page button:focus-visible {
+  outline: 2px solid #96B5A6;
+  outline-offset: 3px;
+}
+
 @media (min-width: 1024px) {
   .patents-public-page > main > section:nth-of-type(6) > div {
     grid-template-columns: minmax(0, 0.92fr) minmax(0, 1.08fr);
@@ -26,8 +32,8 @@ export default function SpanishPatentsPage() {
   return (
     <>
       <PublicPlatformNav active="patents" locale="es" />
-      <style>{patentReportLayoutFix}</style>
-      <div className="patents-public-page [&>main>nav]:hidden">
+      <style>{patentPublicFixes}</style>
+      <div id="main-content" tabIndex={-1} className="patents-public-page [&>main>nav]:hidden focus:outline-none">
         <LocalizedPatentsPage locale="es" />
       </div>
     </>
